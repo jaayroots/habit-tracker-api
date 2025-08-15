23d179ffecf45403c7da813134705610f55f6bb0
jaayroots <chakkapon.jay@gmail.com>
2025-08-15 15:55:28 +0700

    update

--- internal/enums/userType.go
+++ internal/enums/userType.go
@@ -7,33 +7,3 @@ const (
 	Admin      UserType = "admin"
 	SuperAdmin UserType = "superadmin"
 )
-
-// func (s UserType) String() string {
-// 	switch s {
-// 	case User:
-// 		return "user"
-// 	case Admin:
-// 		return "admin"
-// 	case SuperAdmin:
-// 		return "superAdmin"
-// 	default:
-// 		return "unknow"
-// 	}
-// }
-
-// func UserTypeMap() map[int]string {
-// 	return map[int]string{
-// 		int(User):       User.String(),
-// 		int(Admin):      Admin.String(),
-// 		int(SuperAdmin): SuperAdmin.String(),
-// 	}
-// }
-
-// func IsValidUserType(value int) bool {
-// 	switch UserType(value) {
-// 	case User, Admin, SuperAdmin:
-// 		return true
-// 	default:
-// 		return false
-// 	}
-// }
